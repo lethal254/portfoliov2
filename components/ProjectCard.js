c9700d9541fb9662946de43483d9904bf5a55a05
lethal254 <dopelixy2@gmail.com>
2022-08-17 09:40:49 +0300

    Changed project card bg properties

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -11,7 +11,12 @@ const ProjectCard = (props) => {
     <div className='flex items-center rounded-md hover:-translate-y-1 transition-all ease-linear duration-300 min-h-[50vh] md:min-h-[40vh]'>
       <div
         className='w-[100%]  h-full relative overflow-hidden cursor-pointer rounded-md'
-        style={{ backgroundImage: `url("${projectImage.asset.url}")` }}>
+        style={{
+          backgroundImage: `url("${projectImage.asset.url}")`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}>
         <div className='absolute top-0 left-0 bg-slate-900 bg-opacity-90 z-5 w-full h-full border border-1 border-sky-600 border-opacity-30 rounded-md'>
           <div className='px-4 py-4'>
             <h3 className='text-sky-600 text-2xl '>{title}</h3>
